bababc9f800b4d3c0c882acb60be04fb4b82fe48
collazomathias <mathias.collazo.episcopo@gmail.com>
2022-03-28 00:50:41 -0300

    API consumed. Added CSS to elements.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,44 @@
+import React, { useEffect, useState } from "react";
+
+import { Filters } from "./components/filters/Filters.jsx";
+import { Elements } from "./components/elements/Elements.jsx";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "./index.css";
+
+
 function App() {
+
+    let [ pageNumber, setPageNumber ] = useState(1);
+    let [ data, setData ] = useState([]);
+    let { info, results } = data;
+
+    let api_url = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
+    useEffect(() => {
+        (async function() {
+            let data = await fetch(api_url).then((response) => response.json());
+            setData(data);
+        })();
+    }, [api_url])
+
     return (
-        <h1>Hola mundo!</h1>
+        <>
+            <h1 className="text-center ubuntu my-5 fw-bold">Ricky <span className="text-primary">&</span> Morty</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-3">
+                        <Filters />
+                    </div>
+                    <div className="col-8">
+                        <div className="row">
+                            <Elements results={results} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
---
+++ src/components/elements/Elements.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import styles from "./Elements.module.scss";
+
+
+
+export const Elements = ({ results = [] }) => {
+    let display;
+    if(results){
+        display = results.map(element => {
+            let { id, image, name, location, status } = element;
+            return (
+                <div key={id} className="col-4 mb-4 position-relative">
+                    <div className={styles.elements}>
+                        <img src={image} alt="" className={`${styles.image} img-fluid`} />
+                        <div className={styles.content}>
+                            <div className="fs-4 fw-bold mb-4">{name}</div>
+                            <div className="">
+                                <div className="fs-6">Last location:</div>
+                                <div className="fs-5">{location.name}</div>
+                            </div>
+                        </div>
+                    </div>
+                    {(() => {
+                        if(status === "Dead"){
+                            return (
+                                <div className={`${styles.badge} badge bg-danger`}>
+                                    {status}
+                                </div>
+                            );
+                        } else if(status === "Alive") {
+                            return (
+                                <div className={`${styles.badge} badge bg-success`}>
+                                    {status}
+                                </div>
+                            );
+                        } else {
+                            return (
+                                <div className={`${styles.badge} badge bg-secondary`}>
+                                    {status}
+                                </div>
+                            );
+                        }
+                    })()}
+                </div>
+            );
+        });
+    } else {
+        display = "No characters found.";
+    }
+    return <>{display}</>
+}
